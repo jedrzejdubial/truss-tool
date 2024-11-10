f5ba31facb82d96a7726d42d86e1f10f2f20b91c
jedrzejdubial <jedrzejdubial@proton.me>
2024-11-10 19:42:10 +0100

    Code cleanup

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/app/utils/supabase";
+import { functions, supabase } from "@/app/utils/supabase";
 import Button from "@/app/components/Button";
 import { ArrowLeft } from "@phosphor-icons/react";
 import AccountComponent from "@/app/account/AccountComponent";
@@ -14,16 +14,16 @@ interface UserState {
 };
 
 export default function Account() {
-  const [supabaseUser, setSupabaseUser] = useState<UserState>({
+  const [user, setUser] = useState<UserState>({
     picture: "", 
     name: null, 
     email: null
   });
 
   useEffect(() => {
-    const getUserData = async() => {
+    const getUserData = async () => {
       const { data: { user } } = await supabase.auth.getUser();
-      setSupabaseUser({
+      setUser({
         picture: user?.user_metadata.picture || null,
         name: user?.user_metadata.full_name || null,
         email: user?.email || null
@@ -32,51 +32,6 @@ export default function Account() {
 
     getUserData();
   }, []);
-
-  async function signIn() {
-    try {
-      const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google'
-      });
-
-      if(error) throw error;
-    } catch(error) {
-      console.error("Error signing in: " + error);
-      alert("Failed to sign in. Please try again.");
-    }
-  }
-
-  async function signOut() {
-    try {
-      await supabase.auth.signOut();
-      window.location.reload();
-    } catch(error) {
-      console.error("Error signing out:" + error);
-      alert("Failed to sign out. Please try again.");
-    }
-  }
-
-  async function deleteAccount() {
-    try {
-      // Prompt user to confirm their choice
-      if(!window.confirm("Are you sure you want to delete your account? You will lose access to all of your projects.")) return;
-
-      const { data: { user } } = await supabase.auth.getUser();
-      if(!user) throw new Error("No user found");
-
-      /* Delete user data from any other tables first if needed
-      await supabase.from('your_table').delete().eq('user_id', user.id);
-      TODO after actually implementing projects functionality */
-
-      const { error } = await supabase.functions.invoke("delete-user");
-      if(error) throw error;
-
-      await signOut();
-    } catch(error) {
-      console.error("Error deleting account: " + error);
-      alert("Failed to delete your account. Please try again.");
-    }
-  }
 
   return (
     <>
@@ -87,20 +42,20 @@ export default function Account() {
 
 
       <div className="flex flex-col w-4/5 p-8">
-        { supabaseUser.email == null && 
+        { user.email == null && 
         <AccountComponent>
           <h1>You are not signed in</h1>
-          <button onClick={ signIn } className="account_button">Sign in with Google</button>
+          <button onClick={ functions.signIn } className="account_button">Sign in with Google</button>
         </AccountComponent> }
 
-        { supabaseUser.email != null && 
+        { user.email != null && 
         <div>
           <AccountComponent>
             <div className="grid grid-cols-[auto_1fr] gap-8">
-              <Image src={ supabaseUser.picture } alt="Profile picture" width={100} height={100} className="rounded-full" />
+              <Image src={ user.picture } alt="Profile picture" width={100} height={100} className="rounded-full" />
               <div className="grid">
-                <h1>{ supabaseUser.name }</h1>
-                <h2>{ supabaseUser.email }</h2>
+                <h1>{ user.name }</h1>
+                <h2>{ user.email }</h2>
                 <p>Account created with Google</p>
               </div>
             </div>
@@ -108,12 +63,12 @@ export default function Account() {
 
           <AccountComponent>
             <h1>Sign out of your account</h1>
-            <button onClick={ signOut } className="account_button">Sign out</button>
+            <button onClick={ functions.signOut } className="account_button">Sign out</button>
           </AccountComponent>
 
-          <AccountComponent> {/* TODO: make this work */}
+          <AccountComponent>
             <h1>Delete your account</h1>
-            <button onClick={ deleteAccount } className="account_button">Delete</button>
+            <p>If you want to delete your account, please contact jedrzejdubial@proton.me</p>
           </AccountComponent>
         </div> }
       </div>
--- app/utils/supabase/index.ts
+++ app/utils/supabase/index.ts
@@ -4,3 +4,30 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!, 
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export const functions = {
+  signIn: async () => {
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'google'
+      });
+
+      if(error) throw error;
+    } catch(error) {
+      console.error("Error signing in: " + error);
+      alert("Failed to sign in. Please try again.");
+    }
+  },
+
+  signOut: async () => {
+    try {
+      await supabase.auth.signOut();
+
+      // Refresh the page after signing out
+      window.location.reload();
+    } catch(error) {
+      console.error("Error signing out: " + error);
+      alert("Failed to sign out. Please try again.");
+    }
+  }
+};
